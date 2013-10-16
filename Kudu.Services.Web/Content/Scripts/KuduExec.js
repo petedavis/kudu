e06909b5530848c67f1c92c77a64a68373c1b358
Ahmed ElSayed <ahmels@microsoft.com>
2013-10-15 23:20:30 -0700

    making DebugConsole interactive

--- Kudu.Services.Web/Content/Scripts/KuduExec.js
+++ Kudu.Services.Web/Content/Scripts/KuduExec.js
@@ -1,94 +1,107 @@
 var curWorkingDir = ko.observable("");
 window.KuduExec = { workingDir: curWorkingDir };
 
-$(function () {
-    function _changeDir(value) {
-        value = value || window.KuduExec.appRoot;
-        curWorkingDir(value);
-        $(".jquery-console-cursor").parent().prev(".jquery-console-prompt-label").text(value.replace(/\/|\\$/, '') + ">");
-    };
-
-    window.KuduExec.changeDir = _changeDir;
+$(function() {
 
     // call make console after this first command so the current working directory is set.
-    var connection = $.connection('/commandstream'),
-        kuduExecConsole = $('<div class="console">'),
-        curReportFun = function () { },
-        controller;
-
-    connection.start().done(function () {
-        var initial = true;
-        var result = curWorkingDir.subscribe(function (newValue) {
-            // Unsubscribe from listening to the curWorkingDir.
-            result.dispose();
-            window.KuduExec.appRoot = curWorkingDir();
-            controller = kuduExecConsole.console({
-                continuedPrompt: true,
-                promptLabel: function () {
-                    return curWorkingDir() + ">";
-                },
-                commandValidate: function () {
-                    return true;
-                },
-                commandHandle: function (line, reportFn) {
-                    line = line.trim();
-                    curReportFun = reportFn;
-
-                    if (!line) {
-                        reportFn({ msg: "", className: "jquery-console-messae-value" });
-                    } else if (line === "exit" || line === "cls") {
-                        controller.reset();
-                    } else {
-                        _sendCommand(line);
-                        controller.enableInput();
-                    }
-                },
-                cancelHandle: function () {
-                    _sendMessage({ "break": true });
-                    curReportFun("Command canceled by user.", "jquery-console-message-error");
-                },
-                completeHandle: function (line) {
-                    var cdRegex = /^cd\s+(.+)$/,
-                        pathRegex = /.+\s+(.+)/,
-                        matches;
-                    if (matches = line.match(cdRegex)) {
-                        return window.KuduExec.completePath(matches[1], /* dirOnly */ true);
-                    } else if (matches = line.match(pathRegex)) {
-                        return window.KuduExec.completePath(matches[1]);
-                    }
-                    return;
-                },
-                cols: 3,
-                autofocus: true,
-                animateScroll: true,
-                promptHistory: true,
-                welcomeMessage: "Kudu Remote Execution Console\nType 'exit' to reset this console."
-            });
-            $('#KuduExecConsole').append(kuduExecConsole);
-        });
-        _sendCommand("echo.");
-
-        connection.received(function (data) {
-            if (data.CurrentDirectory) {
-                curWorkingDir(data.CurrentDirectory);
-                curReportFun();
-                // _changeDir(data.CurrentDirectory);
-            } else if (controller) {
-                // need to do some massaging of newlines to make it look right
-                var text = (data.Output || data.Error || "").replace(/\r\n/g, '\n'),
-                    className = data.Error ? "jquery-console-message-error" : "jquery-console-message-value";
-                controller.message(text, className, /*ignorePromptBox*/true);
+    var lastLine = "";
+    var lastUserInput = null;
+    var kuduExecConsole = $('<div class="console">');
+    var curReportFun;
+    var controller = kuduExecConsole.console({
+        continuedPrompt: true,
+        promptLabel: function() {
+            return getJSONValue(lastLine);
+        },
+        commandValidate: function() {
+            return true;
+        },
+        commandHandle: function (line, reportFn) {
+            
+            line = line.trim();
+            lastUserInput = line + "\n";
+            curReportFun = reportFn;
+            if (!line) {
+                reportFn({ msg: "", className: "jquery-console-messae-value" });
+            } else if (line === "exit" || line === "cls") {
+                controller.reset();
+                controller.message("", "jquery-console-message-value");
+            } else {
+                _sendCommand(line);
+                controller.enableInput();
             }
-        });
+        },
+        cancelHandle: function() {
+            _sendMessage({ "break": true });
+            curReportFun("Command canceled by user.", "jquery-console-message-error");
+        },
+        completeHandle: function(line) {
+            var cdRegex = /^cd\s+(.+)$/,
+                pathRegex = /.+\s+(.+)/,
+                matches;
+            if (matches = line.match(cdRegex)) {
+                return window.KuduExec.completePath(matches[1], /* dirOnly */ true);
+            } else if (matches = line.match(pathRegex)) {
+                return window.KuduExec.completePath(matches[1]);
+            }
+            return;
+        },
+        cols: 3,
+        autofocus: true,
+        animateScroll: true,
+        promptHistory: true,
+        welcomeMessage: "Kudu Remote Execution Console\r\nType 'exit' to reset this console.\r\n\r\n"
+    });
+    $('#KuduExecConsole').append(kuduExecConsole);
+
+
+    var connection = $.connection('/commandstream');
+
+    connection.start({
+        waitForPageLoad: false,
+        transport: "auto"
     });
 
+    connection.received(function (data) {
+        //if the data has the same class as the last ".jquery-console-message"
+        //then just append it to the last one, if not, create a new div.
+        lastLine = getJSONValue(lastLine);
+        var prompt = getJSONValue(data);
+        var lastConsoleMessage = $(".jquery-console-message").last();
+        lastConsoleMessage.text(lastConsoleMessage.text() + lastLine);
+        $(".jquery-console-inner").append($(".jquery-console-prompt-box"));
+        $(".jquery-console-cursor").parent().prev(".jquery-console-prompt-label").text(prompt);
+        controller.promptText("");
+        controller.scrollToBottom();
+        
+        //Now create the div for the new line that will be printed the next time with the correct class
+        if (data.Error && !lastConsoleMessage.hasClass("jquery-console-message-error")) {
+            controller.message("", "jquery-console-message-error");
+        }
+        //Also if lastline ends with a new line character this means that it's safe to start a new div
+        else if ((data.Output && !lastConsoleMessage.hasClass("jquery-console-message-value")) || endsWith(lastLine, "\n")) {
+            controller.message("", "jquery-console-message-value");
+        }
+        
+        //save last line for next time.
+        lastLine = data;
+    });
+    
     function _sendCommand(input) {
-        _sendMessage({ command: input, dir: curWorkingDir() });
+        _sendMessage(input);
     }
 
     function _sendMessage(input) {
-        connection.send(JSON.stringify(input));
+        connection.send(input);
     }
+    
+    function endsWith(str, suffix) {
+        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    }
+    
+    function getJSONValue(input) {
+        return (input.Output || input.Error || "").toString()
+    }
+
 });
-
-
+    
--- Kudu.Services/Commands/PersistentCommandController.cs
+++ Kudu.Services/Commands/PersistentCommandController.cs
@@ -1,91 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Kudu.Contracts.Tracing;
-using Kudu.Core.Commands;
-using Kudu.Core.Infrastructure;
+using Kudu.Core;
 using Microsoft.AspNet.SignalR;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Kudu.Services
 {
     public class PersistentCommandController : PersistentConnection
     {
-        private readonly ICommandExecutor _commandExecutor;
         private readonly ITracer _tracer;
-        private string _connectionId;
+        private readonly IEnvironment _environment;
+        static readonly IDictionary<string, Process> Processes = new Dictionary<string, Process>();
 
-        public PersistentCommandController(ICommandExecutor commandExecutor, ITracer tracer)
+        public PersistentCommandController(IEnvironment environment, ITracer tracer)
         {
-            _commandExecutor = commandExecutor;
-            _commandExecutor.CommandEvent += OnCommandEvent;
-
+            _environment = environment;
             _tracer = tracer;
         }
 
-        private void OnCommandEvent(CommandEvent evt)
+        protected override Task OnConnected(IRequest request, string connectionId)
         {
-            if (evt.EventType == CommandEventType.Error)
-            {
-                Connection.Send(_connectionId, new { Error = evt.Data });
-            }
-            else if (evt.EventType == CommandEventType.Output)
-            {
-                Connection.Send(_connectionId, new { Output = evt.Data });
-            }
+            var process = GetProcessForConnection(connectionId);
+            var fmt = String.Format("Connected to {0} ({1})\n", process.ProcessName, process.Id);
+            return Connection.Send(connectionId, fmt);
         }
 
-        protected override async Task OnReceived(IRequest request, string connectionId, string data)
+        protected override Task OnDisconnected(IRequest request, string connectionId)
         {
-            _connectionId = connectionId;
-            JObject input = JsonConvert.DeserializeObject<JObject>(data);
-            string command = input.Value<string>("command");
-            string workingDirectory = input.Value<string>("dir");
-            bool breakExecution = input.Value<bool>("break");
-
-            if (_commandExecutor.Executing)
+            try
             {
-                if (breakExecution)
+                var process = GetProcessForConnection(connectionId);
+                process.StandardInput.WriteLine("exit");
+                process.StandardInput.Flush();
+                Thread.Sleep(2000);
+                if (!process.HasExited)
                 {
-                    _commandExecutor.CancelCommand();
-                }
-                else if (!String.IsNullOrEmpty(command))
-                {
-                    await _commandExecutor.SendInput(command);
+                    process.Kill();
                 }
             }
-            else 
+            catch (Exception exception)
             {
-                await ExecProcess(connectionId, command, workingDirectory);
+                _tracer.TraceError(exception);
             }
+            return base.OnDisconnected(request, connectionId);
         }
 
-        private async Task ExecProcess(string connectionId, string command, string workingDirectory)
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
-            using (_tracer.Step("Executing " + command, new Dictionary<string, string> { { "CWD", workingDirectory } }))
+            return Task.Factory.StartNew(() =>
             {
-                object error = null;
-                try
+                var process = GetProcessForConnection(connectionId);
+                process.StandardInput.WriteLine(data.Replace("\n", ""));
+                process.StandardInput.Flush();
+            });
+        }
+
+        Process GetProcessForConnection(string connectionId)
+        {
+            lock (Processes)
+            {
+                Process ret;
+                if (!Processes.TryGetValue(connectionId, out ret))
                 {
-                    var result = await _commandExecutor.ExecuteCommandAsync(command, workingDirectory, calculateWorkingDir: true);
-                    await Connection.Send(connectionId, result);
+                    ret = StartProcess(connectionId);
+                    Processes.Add(connectionId, ret);
                 }
-                catch (CommandLineException ex)
-                {
-                    _tracer.TraceError(ex);
-                    error = new { ex.ExitCode };
-                }
-                catch (Exception ex)
-                {
-                    _tracer.TraceError(ex);
-                    error = new { Error = ex.ToString(), ExitCode = -1 };
-                }
-                if (error != null)
-                {
-                    await Connection.Send(connectionId, error);
-                }
+                return ret;
             }
         }
+        private Process StartProcess(string connectionId)
+        {
+            var startInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = false,
+                FileName = System.Environment.ExpandEnvironmentVariables(@"%SystemDrive%\Windows\System32\cmd.exe"),
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                WorkingDirectory = _environment.SiteRootPath
+            };
+
+            var process = Process.Start(startInfo);
+            var outputReader = TextReader.Synchronized(process.StandardOutput);
+            process.ErrorDataReceived += (sender, evt) => Connection.Send(connectionId, new { Error = evt.Data });
+            process.Exited += (sender, evt) =>
+            {
+                lock (Processes)
+                {
+                    Processes.Remove(connectionId);
+                }
+                Connection.Send(connectionId, new { Output = "Exited: " + process.ExitCode });
+            };
+            var outputThread = new Thread(() =>
+            {
+                while (!process.HasExited)
+                {
+                    int count;
+                    var buffer = new char[1024];
+                    while ((count = outputReader.Read(buffer, 0, 1024)) > 0)
+                    {
+                        var builder = new StringBuilder();
+                        builder.Append(buffer, 0, count);
+                        string s = builder.ToString();
+                        Connection.Send(connectionId, new { Output = s.Replace("\r\n", "\n")});
+                        buffer = new char[1024];
+                    }
+                    Thread.Sleep(200);
+                }
+            });
+            outputThread.Start();
+            process.EnableRaisingEvents = true;
+            process.BeginErrorReadLine();
+            return process;
+        }
+
+
     }
 }
